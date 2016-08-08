fdd48c89e5a7409f43937467d79a4ee6b5db32c3
albatross69 <ar.abelyan@yandex.ru>
2016-08-08 10:39:11 +0300

    Delete Task2.php

--- Task2.php
+++
@@ -1,8 +0,0 @@
-<?php
-#Задание 2
-    $drawings = 80;
-    $by_felt_pen = 23;
-    $by_pencils = 40;
-    $by_paints = $drawings - ($by_felt_pen + $by_pencils);
-
-    echo 'Красками выполнено '.$drawings.'-('.$by_pencils.'+'.$by_felt_pen.') = '.$by_paints.' рисунков';
