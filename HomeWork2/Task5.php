68982fbafa831b273eca5ba46d36d28da3a788e8
albatross69 <ar.abelyan@yandex.ru>
2016-08-25 13:37:30 +0300

    Add files via upload

--- HomeWork2/Task5.php
+++ HomeWork2/Task5.php
@@ -1,7 +1,25 @@
 <?php
 #Задание5
+    function utf8_strrev($str){
+        preg_match_all('/./us', $str, $ar);
+        return join('',array_reverse($ar[0]));
+    }
+
     function is_palindrom($string)
     {
+        if (mb_detect_encoding($string) == 'UTF-8')
+        {
+            $rev_string = mb_strtolower(utf8_strrev($string));
+            if (str_replace(" ", "", $rev_string) == str_replace(" ", "", mb_strtolower($string)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
         $rev_string = strtolower(strrev($string));//переворачивем и приводим в нижний регистр
 
         if (str_replace(" ", "", $rev_string) == str_replace(" ", "", strtolower($string)))
@@ -15,7 +33,7 @@
     }
 
 
-    if (is_palindrom("A roza upala na lapu Azora"))
+    if (is_palindrom("А роза упала на лапу Азора"))
     {
         function notice()
         {
